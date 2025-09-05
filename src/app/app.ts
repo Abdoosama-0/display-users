19d9474118a99e44d4bfebf5d5cbe20da6b13fcd
abdelrhman osama <abdalrhman.osama.mostafa@gmail.com>
2025-09-05 23:51:21 +0300

    1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Users } from "./components/users/users";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Users],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
